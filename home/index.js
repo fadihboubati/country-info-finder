3e4ed1f1389c7d73b56c0eed2f7c2967cc5c808f
fadihboubati <eng.fadi.hb@gmail.com>
2023-03-11 12:20:33 +0300

    feat: home page gui

--- home/index.js
+++ home/index.js
@@ -2,12 +2,11 @@
 
 require('dotenv').config();
 const DEVMODE = process.env.DEVMODE;
-const { getHelloWorldService } = require('../../services/home.service');
 
 
 async function getHome(req, res, next) {
     try {
-        const msg = 'Hello world';
+        // const msg = 'Hello world';
         // const greetingMessage = await getHelloWorldService(msg);
         // res.sendFile(__dirname, 'view/html/index.html');
         res.status(200).sendFile(__dirname + '/view/index.html');
--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -4,7 +4,8 @@ const express = require('express');
 const router = express.Router();
 
 
-const { getHome } = require('../controllers/home/home.controller');
+// const { getHome } = require('../controllers/home/home.controller');
+const { getHome } = require('../../home/index');
 
 
 router.get('/', getHome);
