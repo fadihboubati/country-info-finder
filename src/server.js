05458738062166a77faab4ad76d1297a33bb04ce
fadihboubati <eng.fadi.hb@gmail.com>
2023-03-11 13:48:46 +0300

    Remove unused files from the codebase

--- src/server.js
+++ src/server.js
@@ -6,24 +6,16 @@ const cors = require('cors');
 app.use(cors());
 
 app.use(express.json());
-app.use(express.static('public'));
-
 
 const handle404Error = require('./error-handlers/404');
 const handle500Error = require('./error-handlers/500');
 
-
-
 const homeRoute = require('./routes/home.routes');
 const apiRoutes = require('./routes/api.routes');
-
-
-
 
 //  ----- Routes -----  //
 app.use('/', homeRoute);
 app.use('/api', apiRoutes);
-
 
 
 //  ----- Error handlers -----  //
@@ -34,7 +26,6 @@ app.use(handle500Error);
 async function start(port, prisma) {
 
     app.listen(port, () => console.log(`The server is running on port: ${port}`));
-
 
     // when we are finished using the prisma instance, we should close the connection with prisma.$disconnect(),
     // if you don't, it could cause an unexpected behavior,
