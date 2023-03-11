1ce5007424be0e9b54a281d457d0d13e7c72b4dd
fadihboubati <eng.fadi.hb@gmail.com>
2023-03-11 12:58:36 +0300

    clean and restructure the home page code

--- home/index.js
+++ home/index.js
@@ -3,22 +3,14 @@
 require('dotenv').config();
 const DEVMODE = process.env.DEVMODE;
 
-
 async function getHome(req, res, next) {
     try {
-        // const msg = 'Hello world';
-        // const greetingMessage = await getHelloWorldService(msg);
-        // res.sendFile(__dirname, 'view/html/index.html');
         res.status(200).sendFile(__dirname + '/view/index.html');
-        // res.redirect('/html/index.html');
-
     } catch (error) {
         res.statusCode = 500;
         next(DEVMODE ? error.message : 'Ops, Something wrong happened :( ');
     }
 }
-
-
 
 module.exports = {
     getHome,
--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -4,7 +4,6 @@ const express = require('express');
 const router = express.Router();
 
 
-// const { getHome } = require('../controllers/home/home.controller');
 const { getHome } = require('../../home/index');
 
 
