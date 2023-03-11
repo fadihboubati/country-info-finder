cc9c8486e4a1b4d75ecf2ed950e48591c7b9d0ba
fadihboubati <eng.fadi.hb@gmail.com>
2023-03-11 11:24:10 +0300

    feat: deploy issue

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -5,17 +5,21 @@ const DEVMODE = process.env.DEVMODE;
 const { getHelloWorldService } = require('../services/home.service');
 
 
-
+const path = require('path');
 async function getHome(req, res, next) {
     try {
         const msg = 'Hello world';
         const greetingMessage = await getHelloWorldService(msg);
-        // res.status(200).sendFile(__dirname + '/index.html');
-        // res.status(200).send('./src/view/index.html');
-        res.redirect('/html/index.html');
 
-        // greetingMessage);
+        // res.status(200).sendFile(__dirname + '/../html/index.html');
+        // res.status(200).sendFile(__dirname + '/my.html');
+        // res.status(200).send('./src/view/index.html');
+
+        res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
+        // res.redirect('/html/index.html'); // works
+
     } catch (error) {
+        res.statusCode = 500;
         next(DEVMODE ? error.message : 'Ops, Something wrong happened :( ');
     }
 }
